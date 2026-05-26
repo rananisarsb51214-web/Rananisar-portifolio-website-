6e0cf61e01b400d49c49284ec652dd6682a6f1c0
Nisar ai studio <235848088+rananisarsb51214-web@users.noreply.github.com>
2026-05-27 01:49:30 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Smooth scrolling for navigation links
+https://github.com/rananisarsb51214-web/Nisaraistudio-vercelapp#@/-/// Smooth scrolling for navigation links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
